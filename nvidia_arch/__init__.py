73269f62bc1839d0c5fe3d48ef7a61b3bcc7f736
rathaROG <10178493+rathaROG@users.noreply.github.com>
2026-03-20 23:52:56 +0100

    Bump version from 0.0.3 to 1.0.0

--- nvidia_arch/__init__.py
+++ nvidia_arch/__init__.py
@@ -7,7 +7,7 @@ A lightweight tool for detecting and querying NVIDIA GPU architectures (SM/CC),
 and generating `-gencode` flags for CUDA builds
 """
 
-__version__ = '0.0.3'
+__version__ = '1.0.0'
 
 from .arches import (
     ALL_ARCHS,
